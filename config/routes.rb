a1bbfb14f9ad3b6350e2ddff3b506909998f1453
EC2 Default User <ec2-user@ip-172-31-1-16.ap-northeast-1.compute.internal>
2025-01-22 11:36:44 +0000

    [Add] book index user editページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
 
   resources :books, only: [:new, :index, :show, :edit, :create, :update, :destroy]
-  resources :users, only: [:new, :create, :show, :edit, :index]
+  resources :users, only: [:new, :create, :show, :edit, :index, :update]
   post '/users/:id', to: 'books#create', as: 'create_book'
   
   get 'homes/about' => 'homes#about', as: 'about'
