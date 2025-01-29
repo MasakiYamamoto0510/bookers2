75e55ffd45a4fe5145d06589eecfe0013d2bd0ec
EC2 Default User <ec2-user@ip-172-31-1-16.ap-northeast-1.compute.internal>
2025-01-29 06:43:29 +0000

    [Finish] RSpecのエラーチェック完了

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show, :edit, :index, :update]
   post '/users/:id', to: 'books#create', as: 'create_book'
   
-  get 'homes/about' => 'homes#about', as: 'about'
+  get 'home/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
