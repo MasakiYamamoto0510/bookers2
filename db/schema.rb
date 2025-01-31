23e6c08e8647a19fdaee880f2588586f7aac3bf9
EC2 Default User <ec2-user@ip-172-31-1-16.ap-northeast-1.compute.internal>
2025-01-31 10:13:19 +0000

    [Finish]

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,6 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-##
 
 ActiveRecord::Schema.define(version: 2025_01_27_053521) do
 
